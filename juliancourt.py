90be84d0eae062c827e5510ccd2b61c0d5902b42
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-26 20:44:46 +1000

    Update juliancourt.py

--- juliancourt.py
+++ juliancourt.py
@@ -88,7 +88,7 @@ class Document:
         verbs = [t.lemma_ for t in cleanDoc if t.pos_ =="VERB"]
         adjectives = [t.lemma_ for t in cleanDoc if t.pos_ == "ADJ"]
         others = [t.lemma_ for t in cleanDoc if t.pos_ != "VERB" and t.pos_ != "NOUN" and t.pos_ != "ADJ" and t.pos_ != "NUM"]
-  
+ 
       
         self.__nounAnalysis(nouns)
         self.__lemmatizerDoco(nouns)
