b24224cca8db901c6e12376231907022d2c4e10f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-27 20:58:22 +1000

    Update AssangeCourtAnalysis.py

--- AssangeCourtAnalysis.py
+++ AssangeCourtAnalysis.py
@@ -117,7 +117,7 @@ class Document:
         transformed_model = model.transform(sents_list) #Transform documents to document-term matrix.
         
         self.weightsDict = dict(zip(model.get_feature_names(), tfidf_vector.idf_))
-    
+   
         #Weight of words per document
         max_val = transformed_model.max(axis=0).toarray().ravel()
         sort_by_tfidf = max_val.argsort()
