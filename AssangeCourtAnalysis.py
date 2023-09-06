6e52941dfe9f16632d2410cec9c3a5f7d22e728f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2023-09-06 22:29:13 +1000

    Add files via upload

--- AssangeCourtAnalysis.py
+++ AssangeCourtAnalysis.py
@@ -17,7 +17,7 @@ from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfpage import PDFPage
 from spacy.matcher import PhraseMatcher, Matcher
 from spacy.tokens import Doc, Span, Token
-from spacy.lemmatizer import Lemmatizer
+#from spacy.lemmatizer import Lemmatizer
 from spacy.tokenizer import Tokenizer
 from spacy.lang.en import English
 from sklearn import decomposition
