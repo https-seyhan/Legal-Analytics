543f42ad1f8cda95d9e509bbfe02c8d3c9f3129f
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-01-30 13:29:41 +1100

    Add files via upload

--- TATE_topic_analysis_using_Spacy.py
+++ TATE_topic_analysis_using_Spacy.py
@@ -11,8 +11,8 @@ def extract_text_from_pdf(pdf_path):
 	laparams = pdfminer.layout.LAParams()
 	setattr(laparams, 'all_texts', True)
         
-    fake_file_handle = io.StringIO()
-    converter = TextConverter(sresource_manager, fake_file_handle, laparams=laparams)
+	fake_file_handle = io.StringIO()
+	converter = TextConverter(sresource_manager, fake_file_handle, laparams=laparams)
 	page_interpreter = PDFPageInterpreter(resource_manager, converter)
 	with open(pdf_path, 'rb') as fh:
          for page in PDFPage.get_pages(fh, 
