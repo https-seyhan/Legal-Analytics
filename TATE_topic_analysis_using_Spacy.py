f8d8d28aff111e1a9fbdf9e2e3409d7cfc3db29e
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-01-30 15:49:06 +1100

    Add files via upload

--- TATE_topic_analysis_using_Spacy.py
+++ TATE_topic_analysis_using_Spacy.py
@@ -27,13 +27,13 @@ def extract_text_from_pdf(pdf_path):
             #print('Text ', text)       
          list.append(text)
          
-     csv_writer = csv.writer(sys.stdout, delimiter='\t')
-     with open(r'/home/saul/Desktop/generative-AI/document_analysis/court_document_topics.txt', 'w') as fp:
-		 for item in text:
-			 # write each item on a new line
-             #item = item + ' '
-             fp.write("%s\n" % item)
-             #fp.write("%s" % item)
+    csv_writer = csv.writer(sys.stdout, delimiter='\t')
+    with open(r'/home/saul/Desktop/generative-AI/document_analysis/court_document_topics.txt', 'w') as fp:
+		for item in text:
+			# write each item on a new line
+            #item = item + ' '
+            fp.write("%s\n" % item)
+            #fp.write("%s" % item)
 
 	return text
 
